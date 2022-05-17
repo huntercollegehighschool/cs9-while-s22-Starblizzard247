9ca6c27363df503d21a8779067a71cd67f1dc3d7
Stefani Myung <103528392+Starblizzard247@users.noreply.github.com>
2022-05-17 15:54:43 +0000

    not sure about 5

--- part5.py
+++ part5.py
@@ -18,3 +18,15 @@ Enter a number or enter 0 to stop: 0
 Number of evens: 3
 
 '''
+num = int(input("Enter a number or enter 0 to stop: "))
+evennum = 0
+while num != 0:
+  if num % 2:
+    evennum = evennum +1
+  elif:
+    evennum = evennum
+  num = int(input("Enter a number or enter 0 to stop: "))
+print("Number of evens: ", evennum)
+  
+    
+  
